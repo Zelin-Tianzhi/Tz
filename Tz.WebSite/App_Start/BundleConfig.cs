249f4dfd50d87be549fe62475b89db4100d24503
zelin <walkingfy@gmail.com>
2017-09-06 18:08:15 +0800

    webapi

--- Tz.WebSite/App_Start/BundleConfig.cs
+++ Tz.WebSite/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace Tz.WebSite
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Css/bootstrap.css",
+                      "~/Content/Css/site.css"));
         }
     }
 }
